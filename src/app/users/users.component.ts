c6e74eac148bbed7c488a60971c461e666376131
Lorat1403 <lorat1403@gmail.com>
2025-11-22 22:06:47 +0200

    add html and css for users component previously

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -6,6 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./users.component.scss']
 })
 export class UsersComponent implements OnInit {
+ 
+  readonly headerLinks = ['link1', 'link3', 'link4'];
+  readonly footerLinks = ['link1', 'link1', 'link3', 'link4'];
+  readonly services = [1, 2, 3, 4, 5, 6]; 
 
   constructor() { }
 
