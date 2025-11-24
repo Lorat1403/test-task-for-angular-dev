da35c1846d0c606b81d0967213bde4fe9af2d734
Lorat1403 <lorat1403@gmail.com>
2025-11-24 17:33:40 +0200

    edit User component

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -3,17 +3,14 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
-  styleUrls: ['./users.component.scss']
+  styleUrls: ['./users.component.scss'],
 })
 export class UsersComponent implements OnInit {
- 
-  readonly headerLinks = ['link1', 'link3', 'link4'];
+  readonly headerLinks = ['link1', 'link1', 'link3', 'link4'];
   readonly footerLinks = ['link1', 'link1', 'link3', 'link4'];
-  readonly services = [1, 2, 3, 4, 5, 6]; 
+  readonly services = [1, 2, 3, 4, 5, 6];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- src/app/users/users.module.ts
+++ src/app/users/users.module.ts
@@ -4,12 +4,8 @@ import { CommonModule } from '@angular/common';
 import { UsersRoutingModule } from './users-routing.module';
 import { UsersComponent } from './users.component';
 
-
 @NgModule({
   declarations: [UsersComponent],
-  imports: [
-    CommonModule,
-    UsersRoutingModule
-  ]
+  imports: [CommonModule, UsersRoutingModule],
 })
-export class UsersModule { }
+export class UsersModule {}
